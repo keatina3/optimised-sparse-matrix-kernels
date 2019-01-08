974cde45ec573fae7484f2ae30f0129883a185e0
keatina3 <keatina3@tcd.ie>
2019-01-08 12:19:27 +0000

    added accuracy check in prog.c

--- src/prog.c
+++ src/prog.c
@@ -6,16 +6,18 @@
 #include "DAG.h"
 #include "routines.h"
 
+#define err 1E-15
+
 int main(int argc, char* argv[]){
 	//char* fileA = "../torso1/torso1.mtx";
 	//char* fileb = "../torso1/b_for_torso1.mtx";
 	//char* fileA = "../TSOPF_RS_b678_c2/TSOPF_RS_b678_c2.mtx";
 	//char* fileb = "../TSOPF_RS_b678_c2/b_for_TSOPF_RS_b678_c2_b.mtx";
-	//char* fileA = "../af_0_k101/af_0_k101.mtx";
-	//char* fileb = "../af_0_k101/b_sparse_af_0_k101.mtx";
+	char* fileA = "../af_0_k101/af_0_k101.mtx";
+	char* fileb = "../af_0_k101/b_sparse_af_0_k101.mtx";
 	//char* fileb = "../af_0_k101/b_dense_af_0_k101.mtx";
-	char* fileA = "../test_L.mtx";
-	char* fileb = "../test_b.mtx";
+	//char* fileA = "../test_L.mtx";
+	//char* fileb = "../test_b.mtx";
 	
 	mat_mar A, b, L;
 	real *x,*y;
@@ -35,17 +37,14 @@ int main(int argc, char* argv[]){
 	
 	start = clock();
 	Graph* DG = getReach(&L, &b);
-	//for(i=0;i<b.nz;i++)
-	//	DFS(&L,DG,b.I[i]);
 	y = lsolve_GP(&L, &b, DG);
 	end = clock();
 	time_taken = ((double) (end - start)) / CLOCKS_PER_SEC;
 	printf("Time taken: %lf\n", time_taken);
 	
-	printf("y[n] = %lf, x[n] = %lf\n",y[L.m-1],x[L.m-1]);	
 	for(i=0;i<L.m;i++)
-		if(abs(y[L.m-1]) > 0.0 || abs(x[L.m-1]) > 0.0)
-			printf("y[n] = %lf, x[n] = %lf\n",y[L.m-1],x[L.m-1]);	
+		if(fabs(y[i]-x[i]) > err)
+			printf("y[i] = %0.16lf, x[i] = %0.16lf\n",y[i],x[i]);	
 	
 	real SSE = 0;
 	for(i=0;i<L.m;i++)
